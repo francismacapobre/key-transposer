85c45b54eb40e7ac63f243de42e5210e1e58d5ea
Francis Macapobre <f.macapobre@gmail.com>
2019-06-06 17:44:03 -0700

    End of tutorial 3

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { StyleSheet, Text, View } from 'react-native';
+
+import store from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hey men update!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Hey men update!</Text>
+      </View>
+    </Provider>
   );
 }
 
